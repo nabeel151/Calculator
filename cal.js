f4e23f1b7ef550591a267cf7c4d55c48976e5945
nabeel151 <nabeel.shahbaz22@gmail.com>
2026-04-28 18:13:23 +0500

    my project

---
+++ cal.js
@@ -0,0 +1,52 @@
+let expression = "";
+let isCalculated = false;
+const exp1 = document.getElementById("expression");
+const result1 = document.getElementById("result");
+
+
+function append(value) {
+
+  if (isCalculated) {
+    expression = "";
+    document.getElementById("result").innerText = "0";
+    isCalculated = false;
+  }
+  expression += value;
+  updateDisplay();
+}
+
+function clearAll() {
+  expression = "";
+  document.getElementById("result").innerText = "0";
+  isCalculated = false;
+  updateDisplay();
+
+}
+function deleteLast() {
+  expression = expression.slice(0, -1);
+  updateDisplay();
+
+
+}
+
+
+function updateDisplay() {
+  document.getElementById("expression").innerText = expression || "0";
+
+}
+
+
+
+function calculate() {
+  try {
+    let result = eval(expression);
+    document.getElementById("result").innerText = result;
+    isCalculated = true;
+  }
+  catch {
+    document.getElementById("result").innerText = "error";
+    isCalculated = true;
+  }
+
+
+}
